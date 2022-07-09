b95e46c486d45d4a883080bcf7585c9157b5c987
Tyler <tyler42294@gmail.com>
2022-07-09 10:26:28 -0400

    Add client socket.

--- piClient/main.py
+++ piClient/main.py
@@ -1,5 +1,20 @@
 import RPi.GPIO as GPIO
 import time
+import socket
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Get IP and PORT from env.
+PORT = int(os.getenv('PORT'))
+HOST = os.getenv('HOST')
+
+# Create a socket object
+s = socket.socket()
+
+# connect to the server on local computer
+s.connect((HOST, PORT))
 
 #set GPIO numbering mode
 GPIO.setmode(GPIO.BOARD)
@@ -11,12 +26,19 @@ servo1 = GPIO.PWM(11,50)
 #start PWM running, but with value of 0 (off)
 servo1.start(0)
 
-print("Moving!")
-servo1.ChangeDutyCycle(2)
-time.sleep(5)
+#print("goodbye")
+# servo1.stop()
+#GPIO.cleanup()
 
+def main():
+    # receive data from the server and decoding to get the string.
+    try:
+        duty = (int(s.recv(1024).decode()))
+        print("Moving!")
+        servo1.ChangeDutyCycle(duty)
+    except ValueError:
+        pass
 
-
-print("goodbye")
-servo1.stop()
-GPIO.cleanup()
+if __name__=='__main__':
+    while True:
+        main()
