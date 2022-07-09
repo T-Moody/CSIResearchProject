0e6547513c7b8281883cbdeaa5880d72d7b44db0
Tyler <tyler42294@gmail.com>
2022-07-09 11:07:30 -0400

    Client recieves an int from server and moves the motor depending on the number. Program exits on 0.

--- piClient/main.py
+++ piClient/main.py
@@ -15,7 +15,7 @@ s = socket.socket()
 
 # connect to the server on local computer
 s.connect((HOST, PORT))
-
+GPIO.setwarnings(False)
 #set GPIO numbering mode
 GPIO.setmode(GPIO.BOARD)
 
@@ -26,19 +26,25 @@ servo1 = GPIO.PWM(11,50)
 #start PWM running, but with value of 0 (off)
 servo1.start(0)
 
-#print("goodbye")
-# servo1.stop()
-#GPIO.cleanup()
-
 def main():
-    # receive data from the server and decoding to get the string.
+    # receive data from the server and decoding to get the int value.
     try:
-        duty = (int(s.recv(1024).decode()))
+        duty = (int(s.recv(1024).decode(), 0))
+        print("Duty: " + str(duty))
+        
+        # Shutdown if 0 is recieve from server.
+        if duty == 0 :
+            servo1.stop()
+            GPIO.cleanup()
+            exit()
+        
+        # Move motor.
         print("Moving!")
         servo1.ChangeDutyCycle(duty)
+        
     except ValueError:
         pass
 
-if __name__=='__main__':
+if __name__ == '__main__':
     while True:
         main()
